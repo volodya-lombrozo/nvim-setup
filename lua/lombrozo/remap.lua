d2f14b25a93458a96ff5e5e0ac13fc25d7c27de8
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-04-30 10:58:54 +0300

    feat(#main): Add leader mapping for LSP code actions

--- lua/lombrozo/remap.lua
+++ lua/lombrozo/remap.lua
@@ -2,5 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action' })
 vim.keymap.set("n", "<S-Tab>", "<C-^>", { noremap = true, silent = true, desc = "Toggle alternate file" })
 
