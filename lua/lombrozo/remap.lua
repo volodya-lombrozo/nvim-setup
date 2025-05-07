a14cf6a5976015f3f10b9c461a47fd281650a3bc
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-05-07 18:04:36 +0300

    feat(#main): Add clipboard keymaps for visual mode

--- lua/lombrozo/remap.lua
+++ lua/lombrozo/remap.lua
@@ -4,4 +4,6 @@ vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, { noremap = true, s
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action' })
 vim.keymap.set("n", "<S-Tab>", "<C-^>", { noremap = true, silent = true, desc = "Toggle alternate file" })
+vim.keymap.set("v", "<C-c>", "\"+y", {  desc = "Copy to clipboard" })
+vim.keymap.set("v", "<C-v>", "\"+p", {  desc = "Past from clipboard" })
 
