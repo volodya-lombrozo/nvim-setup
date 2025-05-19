23fd9fedfaf92f12c88fb861b9ce0c28a90ca523
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-05-19 12:36:07 +0300

    chore(#main): Update plugin commits and change leader mapping

--- lua/lombrozo/remap.lua
+++ lua/lombrozo/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action' })
