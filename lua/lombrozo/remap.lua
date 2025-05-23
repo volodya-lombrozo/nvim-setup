26292b31beab205e4345b6302340202b2cb3866f
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-05-23 11:35:58 +0300

    feat(#main): Add leader mappings for yanking entire file

--- lua/lombrozo/remap.lua
+++ lua/lombrozo/remap.lua
@@ -7,4 +7,6 @@ vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "<S-Tab>", "<C-^>", { noremap = true, silent = true, desc = "Toggle alternate file" })
 vim.keymap.set("v", "<C-c>", "\"+y", {  desc = "Copy to clipboard" })
 vim.keymap.set("v", "<C-v>", "\"+p", {  desc = "Past from clipboard" })
+vim.keymap.set("n", "<leader>y", ":%y<CR>", {  desc = "Yank the entire file" })
+vim.keymap.set("n", "<leader>Y", ":%y+<CR>", {  desc = "Yank the entire file to the system clipboard" })
 
