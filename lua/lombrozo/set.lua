d358f703d2da13c1605178bd35ee6d88b914ed3c
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-09-16 19:47:21 +0300

    chore(#main): Update dependencies and enhance visual highlight settings

--- lua/lombrozo/set.lua
+++ lua/lombrozo/set.lua
@@ -24,9 +24,8 @@ vim.opt.relativenumber = true
 
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
-        -- vim.cmd("highlight CursorLine ctermbg=238 guibg=#3a3a3a")
-        -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#3a3a3a", fg = "#3a3a3a" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#3a3a3a" })
         vim.api.nvim_set_hl(0, "Pmenu", { bg = "#3a3a3a", fg = "#c0caf5" })
+        vim.api.nvim_set_hl(0, "Visual", { bg = "#8a8a8a", fg = "NONE" })
     end,
 })
