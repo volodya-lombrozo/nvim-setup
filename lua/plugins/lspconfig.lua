c9dd7fe14766ac5397b35795c6887ecb80efa80f
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-09-18 15:55:47 +0300

    refactor(#main): Update LSP configuration for gopls and groovyls setup

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,28 +1,32 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
-        local lspconfig = require("lspconfig")
-
         -- Make LSP completions work with nvim-cmp
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        -- Set up gopls for Go
-        lspconfig.gopls.setup({
-            capabilities = capabilities,
-        })
-
-        -- Set up groovyls for Groovy
-        lspconfig.groovyls.setup({
-            cmd = { "java", "-jar", "/Users/lombrozo/Workspace/OpenSource/groovy-language-server/build/libs/groovy-language-server-all.jar" },
-        })
-
-        -- Set up tsserver for Lua 
-        lspconfig.lua_ls.setup{}
-
-        -- solargraph for Ruby
-        lspconfig.solargraph.setup({
-            capabilities = capabilities,
+        vim.lsp.config.gopls = {
+            cmd = { "gopls" },
+            filetypes = { "go", "gomod", "gowork", "gotmpl" },
             settings = {
+                capabilities = capabilities,
+            },
+        }
+        vim.lsp.config.lua_ls = {
+            settings = {
+                capabilities = capabilities,
+            }
+        }
+        vim.lsp.config.groovyls = {
+            cmd = { "java", "-jar", "/Users/lombrozo/Workspace/OpenSource/groovy-language-server/build/libs/groovy-language-server-all.jar" },
+            filetypes = { "groovy" },
+            settings = {
+                capabilities = capabilities,
+            }
+        }
+        vim.lsp.config.solargraph = {
+            cmd = { "solargraph", "stdio" },
+            filetypes = { "ruby" },
+            settings = {
+                capabilities = capabilities,
                 solargraph = {
                     diagnostics = true,
                     formatting = true,
@@ -30,9 +34,13 @@ return {
                     hover = true,
                     symbols = true,
                 },
-            },
+            }
+        }
+        vim.lsp.enable({
+            'gopls',
+            'lua_ls',
+            'groovyls',
+            'solargraph'
         })
-
     end
 }
-
