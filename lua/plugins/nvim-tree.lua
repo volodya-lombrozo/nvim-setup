93535d81b02d53f46bbac31f6bbbc3cf40620916
volodya-lombrozo <volodya.lombrozo@gmail.com>
2026-01-31 16:42:16 +0300

    feat(#main): update copilot.lua and nvim-tree.lua configurations

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,6 +5,10 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
+      local function get_tree_width()
+        return math.max(25, math.min(50, vim.fn.winwidth(0) / 5))
+      end
+
       require("nvim-tree").setup({
         -- Close the tree when you open a file
         actions = {
@@ -15,6 +19,9 @@ return {
         -- Don't show netrw (so nvim-tree is default)
         disable_netrw = true,
         hijack_netrw = true,
+        view = {
+          width = get_tree_width(),
+        },
       })
       vim.keymap.set("n", "<leader>pp", ":NvimTreeFindFileToggle<CR>", { silent = true, desc = "Toggle NvimTree" })
     end,
